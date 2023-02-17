6d6a3e38d1e7faeffac789c31848d63d828038e7
Hao Chen <hche8093@uni.sydney.edu.au>
2023-02-17 19:19:56 +1100

    Implement repository

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -8,7 +8,20 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findALl() {}
+  async findALl() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
 
-  async create(message: string) {}
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 10000);
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
